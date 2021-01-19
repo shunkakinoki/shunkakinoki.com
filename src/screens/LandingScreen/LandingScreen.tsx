c441aa4957c2c44236f0d072383c7255146d5e06
Shun Kakinoki <shunkakinoki@gmail.com>
2021-01-19 20:41:33 +0900

    perf: ⚡️ ini personal intro

--- src/screens/LandingScreen/LandingScreen.tsx
+++ src/screens/LandingScreen/LandingScreen.tsx
@@ -5,6 +5,21 @@ export default function LandingScreen(): JSX.Element {
   return (
     <>
       <Header />
+      <div className="flex flex-col items-start justify-center max-w-2xl mx-auto mb-16">
+        <div className="px-3 md:px-0">
+          <h1 className="mb-4 text-3xl font-bold tracking-tight text-black md:text-5xl lg:text-6xl dark:text-white">
+            Hi, I’m Shun Kakinoki.
+          </h1>
+          <h2 className="text-base text-gray-600 md:text-lg dark:text-gray-400">
+            I am an entrepreneur striving to obliterate the galaxy. I have spent
+            my childhood years in Silicon Valley (Cupertino City) from 4 to 10
+            years old and have 3+ years of experience in AI/CV/Deep Learning,
+            Mobile/Web Development &amp; Devops/Infrastructure. Love open source
+            and extremely passionate about the potential of startups towards the
+            future of the world.
+          </h2>
+        </div>
+      </div>
       <Footer />
     </>
   );
