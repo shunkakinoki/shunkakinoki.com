b1cf2d7279bf9f457f9ec1e95040176f3e0bbf44
Shun Kakinoki <shunkakinoki@gmail.com>
2021-01-19 19:08:42 +0900

    perf: ⚡️ ini default theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,7 @@ import { ThemeProvider } from "next-themes";
 
 const CustomApp = ({ Component, pageProps }: AppProps): JSX.Element => {
   return (
-    <ThemeProvider attribute="class">
+    <ThemeProvider attribute="class" defaultTheme="system">
       <Component {...pageProps} />
     </ThemeProvider>
   );
