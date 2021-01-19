43b0abb42c1dd335e2a3fe9e7b3e97dc214eb127
Shun Kakinoki <shunkakinoki@gmail.com>
2021-01-19 11:19:55 +0900

    perf: ⚡️ complete eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,50 @@
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    browser: true,
+    node: true,
+  },
+  extends: ["prettier", "plugin:prettier/recommended"],
+  overrides: [
+    {
+      files: ["next-env.d.ts", "src/**/*.ts", "src/**/*.tsx"],
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        parserOptions: {
+          ecmaVersion: 2021,
+          sourceType: "module",
+          ecmaFeatures: { jsx: true },
+        },
+        project: "tsconfig.json",
+      },
+      settings: {
+        "import/resolver": {
+          typescript: {},
+        },
+        react: {
+          version: "detect",
+        },
+      },
+      plugins: ["@typescript-eslint"],
+      extends: [
+        "plugin:@next/eslint-plugin-next/recommended",
+        "plugin:@typescript-eslint/eslint-recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+        "plugin:jsx-a11y/recommended",
+        "plugin:import/errors",
+        "plugin:import/typescript",
+        "plugin:import/warnings",
+        "plugin:prettier/recommended",
+        "plugin:react/recommended",
+        "plugin:react-hooks/recommended",
+        "plugin:tailwind/recommended",
+        "prettier/@typescript-eslint",
+      ],
+      rules: {
+        "react/react-in-jsx-scope": 0,
+      },
+    },
+  ],
+};
