09d500f3426719b00e8fcac8fab5474a14d7bbb2
Shun Kakinoki <shunkakinoki@gmail.com>
2021-01-19 11:20:36 +0900

    style: 💄 refactor with eslint

--- src/components/LandingScreen/LandingScreen.tsx
+++ src/components/LandingScreen/LandingScreen.tsx
@@ -1,7 +1,7 @@
 export default function LandingScreen(): JSX.Element {
   return (
     <>
-      <h1 className="dark:bg-black">hello world</h1>
+      <h1 className="dark:bg-black ">hello world</h1>
     </>
   );
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,13 @@
 import { AppProps } from "next/app";
 
 import "@/styles/index.css";
+import { ThemeProvider } from "next-themes";
 
 const CustomApp = ({ Component, pageProps }: AppProps): JSX.Element => {
   return (
-    <>
+    <ThemeProvider attribute="class">
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   );
 };
 
