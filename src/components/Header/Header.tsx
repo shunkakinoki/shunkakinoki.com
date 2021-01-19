dc26fbe9f3065b957a21a1b19ce8bc92d8a4f43d
Shun Kakinoki <shunkakinoki@gmail.com>
2021-01-19 19:07:35 +0900

    feat: 🎸 ini dark mode

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -11,7 +11,7 @@ export function HeaderLink({ children, href }: HeaderLinkProps): JSX.Element {
     <li className="px-3 leading-5 text-left text-gray-400 align-baseline hover:text-gray-600 dark:text-gray-500 dark:hover:text-gray-200">
       <Link href={href}>
         {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-        <a className="text-sm font-medium no-underline uppercase cursor-pointer ">
+        <a className="text-xs font-medium no-underline uppercase cursor-pointer md:text-sm lg:text-md">
           {children}
         </a>
       </Link>
@@ -26,7 +26,7 @@ export default function Header(): JSX.Element {
         role="banner"
         className="box-border sticky z-30 flex items-center justify-between w-full max-w-3xl p-8 px-6 py-3 mx-auto my-0 mb-3 leading-5 text-gray-400 align-baseline transition ease-in sm:mb-6 sticky-nav md:my-8 bg-opacity-60"
       >
-        <div className="items-center flex-grow flex-shrink-0 max-w-screen-sm align-baseline">
+        <div className="flex items-center flex-grow flex-shrink-0 max-w-screen-sm align-baseline">
           <span className="pr-2 sm:pr-3 md:pr-5">
             <DarkModeButton></DarkModeButton>
           </span>
@@ -34,14 +34,14 @@ export default function Header(): JSX.Element {
             {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
             <a
               aria-current="page"
-              className="text-xl font-medium leading-loose text-gray-500 no-underline align-baseline hover:text-gray-800 dark:text-gray-400 dark:hover:text-gray-100"
+              className="text-base text-gray-500 lg:text-2xl md:text-xl hover:text-gray-800 dark:text-gray-400 dark:hover:text-gray-100"
             >
               shunkakinoki
             </a>
           </Link>
         </div>
         <nav className="flex-grow flex-shrink-0 block align-baseline">
-          <ul className="flex items-center justify-end leading-5 ">
+          <ul className="flex items-center justify-end leading-5">
             <HeaderLink href="/about">About</HeaderLink>
             <HeaderLink href="/blog">Blog</HeaderLink>
             <HeaderLink href="/dashboard">Dashboard</HeaderLink>
