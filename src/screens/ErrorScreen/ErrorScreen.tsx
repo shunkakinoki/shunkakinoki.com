dc3e590546b82bc865ef2c80917a46926681d45f
Shun Kakinoki <shunkakinoki@gmail.com>
2021-01-19 20:28:06 +0900

    perf: ⚡️ refactor home button for error screen

--- src/screens/ErrorScreen/ErrorScreen.tsx
+++ src/screens/ErrorScreen/ErrorScreen.tsx
@@ -12,7 +12,7 @@ export default function ErrorScreen(): JSX.Element {
         </h1>
         <Link href="/">
           {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-          <a className="inline-flex items-center px-6 py-3 text-base font-medium text-indigo-700 bg-indigo-100 border border-transparent rounded-md hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+          <a className="inline-flex items-center px-6 py-3 text-base font-medium text-indigo-700 bg-indigo-100 border border-transparent rounded-md hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:bg-indigo-600 dark:text-indigo-300 dark:hover:bg-indigo-400 dark:hover:text-indigo-200">
             Go home
           </a>
         </Link>
