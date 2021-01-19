2555dd079c098c53e662bee7080fbf40b12ac9d1
Shun Kakinoki <shunkakinoki@gmail.com>
2021-01-19 13:21:35 +0900

    feat: 🎸 ini header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,38 +1,46 @@
+import Link, { LinkProps } from "next/link";
+
+interface HeaderLinkProps extends LinkProps {
+  children: string;
+}
+
+export function HeaderLink({ children, href }: HeaderLinkProps): JSX.Element {
+  return (
+    <li className="px-3 m-0 leading-5 text-left text-gray-400 align-baseline hover:text-gray-600">
+      <Link href={href}>
+        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+        <a className="p-0 text-sm font-medium no-underline uppercase border-0 cursor-pointer">
+          {children}
+        </a>
+      </Link>
+    </li>
+  );
+}
+
 export default function Header(): JSX.Element {
   return (
     <header
       role="banner"
-      className="box-border sticky top-0 left-0 flex items-center px-6 py-3 mx-0 mt-0 mb-2 leading-5 text-gray-400 align-baseline border-0 sm:mb-6"
+      className="box-border sticky flex items-center px-6 py-3 mb-2 leading-5 text-gray-400 align-baseline border-0 sm:mb-6"
     >
-      <div className="items-center flex-grow flex-shrink-0 p-0 m-0 align-baseline">
-        <h1 className="p-0 text-xl font-medium leading-loose text-gray-400 border-0">
-          <a
-            aria-current="page"
-            className="m-0 text-xl no-underline align-baseline cursor-pointer"
-            href="/"
-          >
-            shunkakinoki
-          </a>
+      <div className="items-center flex-grow flex-shrink-0 max-w-screen-sm align-baseline">
+        <h1 className="text-xl font-medium leading-loose text-gray-400 hover:text-gray-600">
+          <Link href="/">
+            {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+            <a
+              aria-current="page"
+              className="text-xl no-underline align-baseline cursor-pointer "
+            >
+              shunkakinoki
+            </a>
+          </Link>
         </h1>
       </div>
-      <nav className="flex-grow flex-shrink-0 block p-0 m-0 align-baseline">
-        <ul className="flex items-center justify-end p-0 leading-5 text-gray-400 border-0">
-          <li className="px-3 m-0 leading-5 text-left align-baseline">
-            <a
-              href="/blog"
-              className="p-0 text-sm font-medium no-underline uppercase border-0 cursor-pointer"
-            >
-              Blogasdfasdf
-            </a>
-          </li>
-          <li className="px-3 m-0 leading-5 text-left align-baseline">
-            <a
-              href="/links"
-              className="p-0 text-sm font-medium no-underline uppercase border-0 cursor-pointer"
-            >
-              Linkss
-            </a>
-          </li>
+      <nav className="flex-grow flex-shrink-0 block align-baseline">
+        <ul className="flex items-center justify-end leading-5 ">
+          <HeaderLink href="/about">About</HeaderLink>
+          <HeaderLink href="/blog">Blog</HeaderLink>
+          <HeaderLink href="/social">Social</HeaderLink>
         </ul>
       </nav>
     </header>
