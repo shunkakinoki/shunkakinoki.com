867bc80ae16bc52c61627e2acd26794c95b2e082
Shun Kakinoki <shunkakinoki@gmail.com>
2021-01-21 11:33:09 +0900

    perf: ⚡️ ini social circle

--- src/icons/Hero/Check.tsx
+++ src/icons/Hero/Check.tsx
@@ -8,7 +8,7 @@ export default function Check(): JSX.Element {
     >
       <path
         fillRule="evenodd"
-        d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+        d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
         clipRule="evenodd"
       />
     </svg>
