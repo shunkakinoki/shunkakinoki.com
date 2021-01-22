46f9d890960c7933da9892d0bb64fab5c506a38e
Shun Kakinoki <shunkakinoki@gmail.com>
2021-01-22 18:51:26 +0900

    perf: ⚡️ ini resume redirect

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
 module.exports = {
   reactStrictMode: true,
+  redirects() {
+    return [
+      {
+        source: "/resume",
+        destination:
+          "https://drive.google.com/file/d/1WORDhsxecjjhElXfXQw8lapxxaaFrKM8/view",
+        permanent: true,
+      },
+    ];
+  },
 };
