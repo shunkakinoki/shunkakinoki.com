0c9f576062fccc5bd58c4f89e8f63c1af6428a28
Shun Kakinoki <shunkakinoki@gmail.com>
2021-01-23 13:53:41 +0900

    perf: ⚡️ ini i18n header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,3 +1,4 @@
+import useTranslation from "next-translate/useTranslation";
 import Link, { LinkProps } from "next/link";
 
 import s from "./Header.module.css";
@@ -10,7 +11,7 @@ interface HeaderLinkProps extends LinkProps {
 
 export function HeaderLink({ children, href }: HeaderLinkProps): JSX.Element {
   return (
-    <li className="px-3 text-left text-gray-500 align-baseline leading-5 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-200">
+    <li className="px-3 leading-5 text-left text-gray-500 align-baseline hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-200">
       <Link href={href}>
         {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
         <a className="text-sm font-medium no-underline cursor-pointer md:text-base lg:text-md">
@@ -22,13 +23,15 @@ export function HeaderLink({ children, href }: HeaderLinkProps): JSX.Element {
 }
 
 export default function Header(): JSX.Element {
+  const { t } = useTranslation("header");
+
   return (
     <nav className={s.glass}>
       <header
         role="banner"
-        className="sticky z-30 flex items-center justify-between w-full max-w-3xl p-8 px-6 py-3 mx-auto my-3 text-gray-400 align-baseline box-border leading-5 transition ease-in md:my-8 bg-opacity-60 md:py-6 sm:py-4"
+        className="box-border sticky z-30 flex items-center justify-between w-full max-w-3xl p-8 px-6 py-3 mx-auto my-3 leading-5 text-gray-400 align-baseline transition ease-in md:my-8 bg-opacity-60 md:py-6 sm:py-4"
       >
-        <div className="flex items-center flex-grow flex-shrink-0 align-baseline max-w-screen-sm">
+        <div className="flex items-center flex-grow flex-shrink-0 max-w-screen-sm align-baseline">
           <span className="pr-2 sm:pr-3 md:pr-5">
             <DarkModeButton />
           </span>
@@ -44,9 +47,11 @@ export default function Header(): JSX.Element {
         </div>
         <nav className="flex-grow flex-shrink-0 block align-baseline">
           <ul className="flex items-center justify-end leading-5">
-            <HeaderLink href="/about">About</HeaderLink>
-            <HeaderLink href="/blog">Blog</HeaderLink>
-            <HeaderLink href="/dashboard">Dashboard</HeaderLink>
+            <HeaderLink href="/about">{t("common:header.about")}</HeaderLink>
+            <HeaderLink href="/blog">{t("common:header.blog")}</HeaderLink>
+            <HeaderLink href="/dashboard">
+              {t("common:header.dashboard")}
+            </HeaderLink>
           </ul>
         </nav>
       </header>
