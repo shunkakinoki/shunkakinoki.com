b470e1a5fab2bf67a1ffbb049788ad0575f613d5
Shun Kakinoki <shunkakinoki@gmail.com>
2021-01-23 16:17:09 +0900

    feat: 🎸 ini recoil package

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { ThemeProvider } from "next-themes";
 import { AppProps } from "next/app";
+import { RecoilRoot } from "recoil";
 
 import "@/styles/index.css";
 import Seo from "@/components/Seo";
@@ -8,9 +9,11 @@ const CustomApp = ({ Component, pageProps }: AppProps): JSX.Element => {
   return (
     <>
       <Seo />
-      <ThemeProvider attribute="class" defaultTheme="system">
-        <Component {...pageProps} />
-      </ThemeProvider>
+      <RecoilRoot>
+        <ThemeProvider attribute="class" defaultTheme="system">
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </RecoilRoot>
     </>
   );
 };
