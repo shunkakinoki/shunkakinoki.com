673ace51b0090ca39ae911947013ead231fbd179
Shun Kakinoki <shunkakinoki@gmail.com>
2021-01-23 09:28:13 +0900

    perf: ⚡️ ini loading newsletter

--- src/components/Newsletter/Newsletter.tsx
+++ src/components/Newsletter/Newsletter.tsx
@@ -2,6 +2,7 @@ import useSWR from "swr";
 import fetcher from "@/lib/fetcher";
 import { Badge, Exclamation, Light } from "@/icons";
 import { useState } from "react";
+import clsx from "clsx";
 
 export default function Newsletter(): JSX.Element {
   const { data } = useSWR<{
@@ -9,12 +10,13 @@ export default function Newsletter(): JSX.Element {
   }>("/api/buttondown", fetcher);
 
   const [form, setForm] = useState<{
-    state: "error" | "success";
+    state: "error" | "success" | "loading";
     message: string;
   }>({ state: "success", message: "" });
 
   const subscribe = async (e: React.FormEvent): Promise<void> => {
     e.preventDefault();
+    setForm({ state: "loading", message: "" });
 
     const formData = new FormData(e.target as HTMLFormElement);
 
@@ -111,9 +113,35 @@ export default function Newsletter(): JSX.Element {
               <div className="mt-4 sm:mt-0 sm:ml-3">
                 <button
                   type="submit"
-                  className="block w-full px-5 py-3 text-base font-medium text-white bg-indigo-500 border border-transparent rounded-md shadow dark:bg-indigo-600 hover:bg-indigo-400 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-indigo-600 sm:px-10 dark:hover:bg-indigo-500"
+                  className={clsx(
+                    "inline-flex items-center w-full px-5 py-3 text-base font-medium text-white bg-indigo-500 border border-transparent rounded-md shadow dark:bg-indigo-600 hover:bg-indigo-400 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-indigo-600 sm:px-10 dark:hover:bg-indigo-500",
+                    "cursor-not-allowed" && form?.state === "loading"
+                  )}
                 >
-                  Subscribe
+                  {form?.state === "loading" && (
+                    <svg
+                      className="w-5 h-5 mr-3 text-white animate-spin"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                    >
+                      <circle
+                        className="opacity-25"
+                        cx="12"
+                        cy="12"
+                        r="10"
+                        stroke="currentColor"
+                        strokeWidth="4"
+                      />
+                      <path
+                        className="opacity-75"
+                        fill="currentColor"
+                        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                      />
+                    </svg>
+                  )}
+                  {form?.state === "loading" && "Loading..."}
+                  {form?.state !== "loading" && "Subscribe"}
                 </button>
               </div>
             </form>
