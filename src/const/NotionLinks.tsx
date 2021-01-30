cdaeaaadc56bcc053eb074f9ff591b1b4d31ec2c
Shun Kakinoki <shunkakinoki@gmail.com>
2021-01-30 15:24:17 +0900

    feat: 🎸 ini notion links

--- src/const/NotionLinks.tsx
+++ src/const/NotionLinks.tsx
@@ -1,5 +1,12 @@
 export default class NotionLinks {
-  static readonly blog = "d9aa23e0aad040ea85f2e319e5204caa";
-  static readonly mission = "a83bc6f977354a0c972a54a0b895177d";
-  static readonly plan = "031019e58f8347b995f00080336417fb";
+  static readonly action = "a172bffd73c643adbe11e6baead25da1";
+  static readonly bible = "3dd2a6e76ec6400b8fda441a8094152d";
+  static readonly diary = "bc9a28b80a3943ac8125546bab652065";
+  static readonly endeavor = "7bf69ff2fa874142bc8e74aea52c2a44";
+  static readonly excerpt = "f00fc4d9b0fa49d2aa871a6be7abb2f4";
+  static readonly habit = "c456c84a06904c018ebd9f556e781291";
+  static readonly insight = "d7f6dc39fcc34e8bb1fba8eaff2b5c2d";
+  static readonly notebook = "38c7e35329d34b97b90040c26682ab0d";
+  static readonly perseverance = "aeb8e5729d3c4ddfaa571413b03fa477";
+  static readonly resource = "637d91c2f74545a29a39e83174ad6cb7";
 }
