7c394a972c20b6bdee094192f5f9e312a1edb5df
Shun Kakinoki <shunkakinoki@gmail.com>
2021-01-30 14:31:32 +0900

    perf: ⚡️ refactor life partial

--- src/components/Life/Life.tsx
+++ src/components/Life/Life.tsx
@@ -25,33 +25,47 @@ export default function Life({ isPartial = false }: Props): JSX.Element {
         <SectionText isPartial={isPartial}>Life</SectionText>
       </div>
       <div className="overflow-hidden bg-gray-200 shadow divide-y divide-gray-200 dark:bg-gray-400 sm:divide-y-0 sm:grid sm:grid-cols-2 sm:gap-0.5 md:grid-cols-3">
-        <LifeCard color="red" name="Action" href="/action">
-          <Action />
-        </LifeCard>
-        <LifeCard color="indigo" name="Bible" href="/bible">
-          <Bible />
-        </LifeCard>
+        {!isPartial && (
+          <LifeCard color="red" name="Action" href="/action">
+            <Action />
+          </LifeCard>
+        )}
+        {!isPartial && (
+          <LifeCard color="indigo" name="Bible" href="/bible">
+            <Bible />
+          </LifeCard>
+        )}
         <LifeCard color="blue" name="Diary" href="/diary">
           <Diary />
         </LifeCard>
-        <LifeCard color="purple" name="Excerpt" href="/excerpt">
-          <Excerpt />
-        </LifeCard>
+        {!isPartial && (
+          <LifeCard color="purple" name="Excerpt" href="/excerpt">
+            <Excerpt />
+          </LifeCard>
+        )}
         <LifeCard color="pink" name="Habit" href="/habit">
           <Habit />
         </LifeCard>
-        <LifeCard color="green" name="Insight" href="/insight">
-          <Insight />
-        </LifeCard>
-        <LifeCard color="gray" name="Notebook" href="/notebook">
-          <Notebook />
-        </LifeCard>
-        <LifeCard color="blue" name="Perseverance" href="/perseverance">
-          <Perseverance />
-        </LifeCard>
-        <LifeCard color="yellow" name="Resource" href="/resource">
-          <Resource />
-        </LifeCard>
+        {!isPartial && (
+          <LifeCard color="green" name="Insight" href="/insight">
+            <Insight />
+          </LifeCard>
+        )}
+        {!isPartial && (
+          <LifeCard color="gray" name="Notebook" href="/notebook">
+            <Notebook />
+          </LifeCard>
+        )}
+        {!isPartial && (
+          <LifeCard color="blue" name="Perseverance" href="/perseverance">
+            <Perseverance />
+          </LifeCard>
+        )}
+        {!isPartial && (
+          <LifeCard color="yellow" name="Resource" href="/resource">
+            <Resource />
+          </LifeCard>
+        )}
       </div>
     </section>
   );
