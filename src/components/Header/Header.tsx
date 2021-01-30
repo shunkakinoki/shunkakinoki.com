7b31657aa872199475002d0b451ad40199253835
Shun Kakinoki <shunkakinoki@gmail.com>
2021-01-30 14:21:45 +0900

    perf: ⚡️ refactor header text-xl

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -15,7 +15,7 @@ interface HeaderLinkProps extends LinkProps {
 
 export function HeaderLink({ children, href }: HeaderLinkProps): JSX.Element {
   return (
-    <li className="px-3 text-left text-gray-500 align-baseline leading-5 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-200">
+    <li className="px-3 leading-5 text-left text-gray-500 align-baseline hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-200">
       <Link href={href}>
         {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
         <a className="text-sm font-medium no-underline cursor-pointer md:text-base lg:text-md">
@@ -33,9 +33,9 @@ export default function Header(): JSX.Element {
     <nav className={s.glass}>
       <header
         role="banner"
-        className="sticky z-30 flex items-center justify-between w-full max-w-3xl p-8 px-6 py-6 mx-auto my-6 text-gray-400 align-baseline box-border leading-5 transition ease-in md:my-8 bg-opacity-60 md:py-6 sm:py-4"
+        className="box-border sticky z-30 flex items-center justify-between w-full max-w-3xl p-8 px-6 py-6 mx-auto my-6 leading-5 text-gray-400 align-baseline transition ease-in md:my-8 bg-opacity-60 md:py-6 sm:py-4"
       >
-        <div className="flex items-center flex-grow flex-shrink-0 align-baseline max-w-screen-sm">
+        <div className="flex items-center flex-grow flex-shrink-0 max-w-screen-sm align-baseline">
           <span className="pr-2 sm:pr-3 md:pr-5">
             <DarkModeButton />
           </span>
@@ -43,7 +43,7 @@ export default function Header(): JSX.Element {
             {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
             <a
               aria-current="page"
-              className="text-base text-transparent lg:text-2xl md:text-xl hover:underline bg-clip-text bg-gradient-to-r from-green-300 via-blue-500 to-purple-600"
+              className="text-xl text-transparent lg:text-2xl hover:underline bg-clip-text bg-gradient-to-r from-green-300 via-blue-500 to-purple-600"
             >
               shunkakinoki
             </a>
