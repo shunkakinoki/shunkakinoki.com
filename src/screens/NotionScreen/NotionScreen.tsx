ac0d68922c077a230230d5170d09f2a75633cb0a
Shun Kakinoki <shunkakinoki@gmail.com>
2021-01-31 20:14:13 +0900

    perf: ⚡️ remove newsletter from notion component

--- src/screens/NotionScreen/NotionScreen.tsx
+++ src/screens/NotionScreen/NotionScreen.tsx
@@ -1,6 +1,5 @@
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
-import Newsletter from "@/components/Newsletter";
 import Notion, { Props as NotionProps } from "@/components/Notion";
 
 export type Props = NotionProps;
@@ -14,7 +13,6 @@ export default function NotionScreen({
       <Header />
       <div className="flex flex-col items-start justify-center max-w-2xl mx-auto">
         <Notion fullPage={fullPage} recordMap={recordMap} />
-        <Newsletter />
       </div>
       <Footer />
     </>
