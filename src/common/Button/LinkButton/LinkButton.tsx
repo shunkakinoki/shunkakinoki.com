229a19886c70289475c6df1f63a128ffd24a5d99
Shun Kakinoki <shunkakinoki@gmail.com>
2021-02-05 13:04:51 +0900

    perf: ⚡️ refactor dark link button text color

--- src/common/Button/LinkButton/LinkButton.tsx
+++ src/common/Button/LinkButton/LinkButton.tsx
@@ -8,7 +8,7 @@ export default function LinkButton({ children }: Props): JSX.Element {
   return (
     <Link href="/">
       {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-      <a className="inline-flex items-center px-6 py-3 text-base font-medium text-indigo-700 bg-indigo-100 border border-transparent rounded-md hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:bg-indigo-600 dark:text-indigo-300 dark:hover:bg-indigo-400 dark:hover:text-indigo-200">
+      <a className="inline-flex items-center px-6 py-3 text-base font-medium text-indigo-700 bg-indigo-100 border border-transparent rounded-md hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:bg-indigo-600 dark:text-indigo-100 dark:hover:bg-indigo-400 dark:hover:text-indigo-50">
         {children}
       </a>
     </Link>
