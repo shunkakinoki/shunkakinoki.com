98aa5cba7f088671986b9054b103cc86269be854
Shun Kakinoki <shunkakinoki@gmail.com>
2021-02-08 22:34:27 +0900

    feat: 🎸 complete refactor redirects

--- next.config.js
+++ next.config.js
@@ -12,9 +12,39 @@ module.exports = nextTranslate({
   redirects() {
     return [
       {
+        source: "/how",
+        destination: "/mission",
+        permanent: true,
+      },
+      {
         source: "/resume",
         destination:
           "https://drive.google.com/file/d/1WORDhsxecjjhElXfXQw8lapxxaaFrKM8/view",
+        permanent: true,
+      },
+      {
+        source: "/what",
+        destination: "/about",
+        permanent: true,
+      },
+      {
+        source: "/when",
+        destination: "/history",
+        permanent: true,
+      },
+      {
+        source: "/where",
+        destination: "https://zen.ly/shunkakinoki",
+        permanent: true,
+      },
+      {
+        source: "/who",
+        destination: "/credits",
+        permanent: true,
+      },
+      {
+        source: "/why",
+        destination: "/value",
         permanent: true,
       },
     ];
