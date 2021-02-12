b43f43b4c02c6453009a1fa6bcee194c1b5b7875
Shun Kakinoki <shunkakinoki@gmail.com>
2021-02-11 17:59:52 -0800

    feat: 🎸 ini about source

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,48 @@
-import { NextPage } from "next";
+import {
+  GetStaticProps,
+  GetStaticPropsContext,
+  InferGetStaticPropsType,
+} from "next";
 
+import { MdxRemote } from "next-mdx-remote/types";
+
+import { getGithubContent } from "@/lib/github";
 import LandingScreen from "@/screens/LandingScreen";
 
-const Index: NextPage = () => {
-  return <LandingScreen />;
+export interface Props {
+  source: string;
+}
+
+export const getStaticProps: GetStaticProps<Props> = async ({
+  locale,
+}: // eslint-disable-next-line @typescript-eslint/require-await
+GetStaticPropsContext) => {
+  try {
+    const result = await getGithubContent("about", "ABOUT", locale, [2, 3]);
+    if (result) {
+      const { source } = result;
+      return {
+        props: {
+          source: JSON.stringify(source),
+        },
+        revalidate: 30,
+      };
+    } else {
+      return {
+        notFound: true,
+      };
+    }
+  } catch {
+    return {
+      notFound: true,
+    };
+  }
+};
+
+const Index = ({
+  source,
+}: InferGetStaticPropsType<typeof getStaticProps>): JSX.Element => {
+  return <LandingScreen source={JSON.parse(source) as MdxRemote.Source} />;
 };
 
 export default Index;
--- src/screens/AboutScreen/AboutScreen.tsx
+++ src/screens/AboutScreen/AboutScreen.tsx
@@ -1,14 +1,17 @@
-import About from "@/components/About";
+import { MdxRemote } from "next-mdx-remote/types";
+
+import About, { Props as AboutProps } from "@/components/About";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import Life from "@/components/Life";
+export type Props = AboutProps;
 
-export default function AboutScreen(): JSX.Element {
+export default function AboutScreen({ source }: Props): JSX.Element {
   return (
     <>
       <Header />
       <div className="flex flex-col items-start justify-center max-w-2xl mx-auto">
-        <About />
+        <About source={source as MdxRemote.Source} />
         <Life />
       </div>
       <Footer />
