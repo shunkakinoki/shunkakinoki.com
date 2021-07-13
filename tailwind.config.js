813e339689316c155675cf1c7a755021be68f226
Shun Kakinoki <shunkakinoki@gmail.com>
2021-07-13 12:20:04 +0900

    perf: upgrade tailwindcss plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,1 @@
-module.exports = {
-  darkMode: "class",
-  mode: "jit",
-  purge: ["src/**/*.{js,ts,jsx,tsx}"],
-  plugins: [
-    require("@tailwindcss/aspect-ratio"),
-    require("@tailwindcss/line-clamp"),
-    require("@tailwindcss/forms"),
-    require("@tailwindcss/typography"),
-  ],
-};
+module.exports = require("@shunkakinoki/tailwindcss");
