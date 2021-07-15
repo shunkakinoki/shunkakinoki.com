5751ccc365a1c936d02eea6887da8e501e01d6f7
Shun Kakinoki <shunkakinoki@gmail.com>
2021-07-16 01:18:29 +0900

    chore: update

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -3,8 +3,13 @@ import "tailwindcss/tailwind.css";
 import I18nProvider from "next-translate/I18nProvider";
 import { ThemeProvider } from "next-themes";
 import { RouterContext } from "next/dist/next-server/lib/router-context";
+
 import config from "../i18n";
+import _error from "../locales/en/_error.json";
+import about from "../locales/en/about.json";
 import common from "../locales/en/common.json";
+import form from "../locales/en/form.json";
+import landing from "../locales/en/landing.json";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
@@ -25,7 +30,11 @@ export const parameters = {
 
 export const decorators = [
   Story => (
-    <I18nProvider lang="en" config={config} namespaces={{ common }}>
+    <I18nProvider
+      lang="en"
+      config={config}
+      namespaces={{ _error, about, common, form, landing }}
+    >
       <RecoilRoot>
         <ThemeProvider attribute="class" defaultTheme="system">
           <Story />
