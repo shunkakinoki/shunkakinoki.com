3bcb6d4acf2be73a4b37bfc3e02a0a8fa63f6c4b
Shun Kakinoki <shunkakinoki@gmail.com>
2021-08-04 23:38:12 -0700

    perf: ini svg favicon

--- src/components/Seo/Seo.tsx
+++ src/components/Seo/Seo.tsx
@@ -1,24 +1,30 @@
 import { DefaultSeo } from "next-seo";
+import Head from "next/head";
 
 export default function Seo(): JSX.Element {
   return (
-    <DefaultSeo
-      noindex={false}
-      nofollow={false}
-      title="Shun Kakinoki"
-      canonical="https://shunkakinoki.com"
-      description="Shun Kakinoki - Obliterate the Galaxy."
-      openGraph={{
-        locale: "en_US",
-        site_name: "shunkakinoki.com",
-        type: "website",
-        url: "https://shunkakinoki.com",
-      }}
-      twitter={{
-        cardType: "summary_large_image",
-        handle: "@shunkakinoki",
-        site: "@shunkakinoki",
-      }}
-    />
+    <>
+      <DefaultSeo
+        noindex={false}
+        nofollow={false}
+        title="Shun Kakinoki"
+        canonical="https://shunkakinoki.com"
+        description="Shun Kakinoki - Obliterate the Galaxy."
+        openGraph={{
+          locale: "en_US",
+          site_name: "shunkakinoki.com",
+          type: "website",
+          url: "https://shunkakinoki.com",
+        }}
+        twitter={{
+          cardType: "summary_large_image",
+          handle: "@shunkakinoki",
+          site: "@shunkakinoki",
+        }}
+      />
+      <Head>
+        <link rel="icon" href="/favicon.svg" />
+      </Head>
+    </>
   );
 }
