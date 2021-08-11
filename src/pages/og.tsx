26a0c22a5b221e6e121c2cf13523b449234a772c
Shun Kakinoki <shunkakinoki@gmail.com>
2021-08-11 01:16:42 -0700

    perf: ini og screen

--- src/pages/og.tsx
+++ src/pages/og.tsx
@@ -1,32 +1,9 @@
-import type { FC } from "react";
+import type { NextPage } from "next";
 
-import { Config } from "@/components/Config";
-import { Viewer } from "@/components/Viewer";
-import { useIsMounted } from "@/hooks/useIsMounted";
+import OgScreen from "@/screens/OgScreen";
 
-export const Index: FC = () => {
-  const isMounted = useIsMounted();
-
-  return (
-    <>
-      <main className="px-6 pb-20 mx-auto w-full max-w-6xl">
-        <header className="mt-20 mb-12 space-y-6 text-center">
-          <h1 className="text-5xl font-bold dark:text-white">
-            Sentrei OG Image Generator
-          </h1>
-          <h2 className="text-xl text-gray-600 dark:text-gray-300">
-            Dynamic open graph images for your website
-          </h2>
-        </header>
-        {isMounted && (
-          <section className="grid grid-cols-1 md:grid-cols-3 md:gap-8 gap-y-8">
-            <Config />
-            <Viewer />
-          </section>
-        )}
-      </main>
-    </>
-  );
+const History: NextPage = () => {
+  return <OgScreen />;
 };
 
-export default Index;
+export default History;
---
+++ src/screens/OgScreen/OgScreen.tsx
@@ -0,0 +1,31 @@
+import useTranslation from "next-translate/useTranslation";
+import type { FC } from "react";
+
+import { Config } from "@/components/Config";
+import { Viewer } from "@/components/Viewer";
+import { useIsMounted } from "@/hooks/useIsMounted";
+
+export const OgScreen: FC = () => {
+  const isMounted = useIsMounted();
+  const { t } = useTranslation();
+
+  return (
+    <>
+      <main className="px-6 pb-20 mx-auto w-full max-w-6xl">
+        <header className="mt-20 mb-12 space-y-6 text-center">
+          <h1 className="text-5xl font-bold dark:text-white">
+            {t("common:header.og")}
+          </h1>
+        </header>
+        {isMounted && (
+          <section className="grid grid-cols-1 md:grid-cols-3 md:gap-8 gap-y-8">
+            <Config />
+            <Viewer />
+          </section>
+        )}
+      </main>
+    </>
+  );
+};
+
+export default OgScreen;
