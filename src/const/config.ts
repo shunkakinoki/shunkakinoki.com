de6997689bf7dfb1083a869e372343f86a4d52a6
Shun Kakinoki <shunkakinoki@gmail.com>
2021-08-11 00:23:41 -0700

    perf: refactor lib const

--- src/const/config.ts
+++ src/const/config.ts
@@ -1,8 +1,6 @@
 import type { IConfig } from "next-og-utils";
 
-import { layouts } from "@/layouts";
-
 export const DEFAULT_CONFIG: IConfig = {
   fileType: "png",
-  layoutName: layouts[0].name,
+  layoutName: "Blog",
 };
---
+++ src/lib/getHtml.tsx
@@ -0,0 +1,38 @@
+import { getCommonCSS } from "next-og-utils";
+import type { IConfig, ILayoutConfig, LayoutComponent } from "next-og-utils";
+import { renderToString } from "react-dom/server";
+
+import { layouts } from "@/layouts";
+
+const NotImplemented: LayoutComponent = ({ config }) => {
+  return <h1 style={{ fontSize: 100 }}>{config.layoutName} not implemented</h1>;
+};
+
+export const getHtml = (config: IConfig & ILayoutConfig): string => {
+  const layout = layouts.find(l => {
+    return l.name === config.layoutName;
+  });
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+  const rendered = renderToString(
+    layout?.Component != null ? (
+      <layout.Component config={config} />
+    ) : (
+      <NotImplemented config={config} />
+    ),
+  );
+
+  return `<!DOCTYPE html>
+<html>
+    <meta charset="utf-8">
+    <title>Generated Image</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <style>
+      ${getCommonCSS()}
+      ${layout?.getCSS != null ? layout.getCSS(config) : ""}
+    </style>
+    <body>
+      ${rendered}
+    </body>
+</html>`;
+};
