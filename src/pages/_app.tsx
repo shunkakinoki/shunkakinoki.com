dbc8cf79ad62200ec80fb433732222810a9fb19f
Shun Kakinoki <shunkakinoki@gmail.com>
2021-08-11 23:06:02 -0700

    chore: update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { ThemeProvider } from "next-themes";
 import type { AppProps } from "next/app";
+import Script from "next/script";
 import { RecoilRoot } from "recoil";
 
 import "tailwindcss/tailwind.css";
@@ -8,6 +9,11 @@ import { Default } from "@/components/Seo";
 const CustomApp = ({ Component, pageProps }: AppProps): JSX.Element => {
   return (
     <>
+      <Script
+        defer
+        src="https://static.cloudflareinsights.com/beacon.min.js"
+        data-cf-beacon='{"token": "aadfc302c28748169bacfd73215ca576"}'
+      />
       <Default />
       <RecoilRoot>
         <ThemeProvider attribute="class" defaultTheme="system">
