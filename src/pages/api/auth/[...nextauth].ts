e012f25ce367c6c3805abec50dcd25a4b71c4d04
Shun Kakinoki <shunkakinoki@gmail.com>
2021-08-17 10:38:57 -0700

    perf: ini auth api

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -7,9 +7,9 @@ const prisma = new PrismaClient();
 
 export default NextAuth({
   providers: [
-    Providers.Google({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    Providers.GitHub({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
     }),
   ],
   adapter: PrismaAdapter(prisma),
