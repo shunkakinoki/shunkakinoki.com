c0082435c0083d19cb6d7a048206f38ee946c803
Shun Kakinoki <shunkakinoki@gmail.com>
2021-08-17 12:49:33 -0700

    perf: ini profile auth

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,8 +1,14 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
+import type { User as NextAuthUser } from "next-auth";
 import NextAuth from "next-auth";
 import Providers from "next-auth/providers";
 
+interface NextAuthUserWithStringId extends NextAuthUser {
+  id: string;
+}
 const prisma = new PrismaClient();
 
 export const Auth = NextAuth({
@@ -10,6 +16,15 @@ export const Auth = NextAuth({
     Providers.GitHub({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      profile(profile) {
+        return {
+          //@ts-expect-error Until next-auth v4
+          id: profile.id.toString(),
+          name: profile.name || profile.login,
+          email: profile.email,
+          image: profile.avatar_url,
+        } as NextAuthUserWithStringId;
+      },
     }),
   ],
   adapter: PrismaAdapter(prisma),
