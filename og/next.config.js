552cbd4137afd679a88090b7cfd1e5e63ab91b68
Shun Kakinoki <shunkakinoki@gmail.com>
2021-08-21 10:52:52 -0700

    chore: update

--- og/next.config.js
+++ og/next.config.js
@@ -1,5 +1,3 @@
-const TerserPlugin = require("terser-webpack-plugin");
-
 /**
  * @type {import('next/dist/next-server/server/config').NextConfig}
  **/
@@ -7,25 +5,5 @@ module.exports = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  webpack: (config, { dev, isServer }) => {
-    if (isServer && !dev) {
-      config.optimization = {
-        minimize: true,
-        minimizer: [
-          new TerserPlugin({
-            parallel: true,
-            terserOptions: {
-              output: { comments: false },
-              mangle: true,
-              compress: true,
-            },
-            extractComments: false,
-          }),
-        ],
-      };
-    }
-    return config;
-  },
-
   trailingSlash: false,
 };
