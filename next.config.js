2507b5b529d7180bd28ce009318aa8c4575cc1ca
Shun Kakinoki <shunkakinoki@gmail.com>
2021-08-24 09:42:32 -0700

    chore: update

--- next.config.js
+++ next.config.js
@@ -6,6 +6,28 @@ const withTranslate = require("next-translate");
  * @type {import('next/dist/next-server/server/config').NextConfig}
  **/
 const config = {
+  cleanDistDir: true,
+  experimental: {
+    conformance: true,
+    craCompat: false,
+    cpus: 1,
+    disableOptimizedLoading: false,
+    externalDir: true,
+    gzipSize: true,
+    esmExternals: true,
+    optimizeCss: true,
+    optimizeFonts: true,
+    optimizeImages: true,
+    pageEnv: true,
+    plugins: true,
+    profiling: true,
+    reactRoot: false,
+    scriptLoader: true,
+    scrollRestoration: true,
+    sprFlushToDisk: true,
+    stats: true,
+    workerThreads: true,
+  },
   future: {
     strictPostcssConfiguration: true,
   },
@@ -17,7 +39,45 @@ const config = {
     defaultLocale: "en",
     localeDetection: true,
   },
+  reactStrictMode: true,
+  poweredByHeader: true,
+  productionBrowserSourceMaps: true,
+  redirects() {
+    return [
+      {
+        source: "/how",
+        destination: "/mission",
+        permanent: true,
+      },
+      {
+        source: "/what",
+        destination: "/values",
+        permanent: true,
+      },
+      {
+        source: "/when",
+        destination: "/history",
+        permanent: true,
+      },
+      {
+        source: "/who",
+        destination: "/about",
+        permanent: true,
+      },
+      {
+        source: "/why",
+        destination: "/cause",
+        permanent: true,
+      },
+      {
+        source: "/where",
+        destination: "https://zen.ly/shunkakinoki",
+        permanent: true,
+      },
+    ];
+  },
   trailingSlash: false,
+  webpack5: true,
 };
 
 const plugins = [
