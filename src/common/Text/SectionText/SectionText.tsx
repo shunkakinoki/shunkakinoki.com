01ff57c0105858706cd282af0cc1a49c09d4178e
Shun Kakinoki <shunkakinoki@gmail.com>
2021-08-23 19:43:05 -0700

    chore: update

--- src/common/Text/SectionText/SectionText.tsx
+++ src/common/Text/SectionText/SectionText.tsx
@@ -10,7 +10,7 @@ export const SectionText: FC<Props> = ({ children, isPartial = false }) => {
   return (
     <h3
       className={clsx(
-        "my-3 text-lg font-medium leading-loose text-gray-800 dark:text-gray-300 align-baseline",
+        "my-3 text-lg font-medium leading-loose text-warmGray-900 dark:text-coolGray-100 align-baseline",
         isPartial && "md:text-xl",
         !isPartial && "md:my-6 md:text-3xl",
       )}
