9db5b6c6eaee6f49eb3cb346050d58674a94e7c3
Shun Kakinoki <39187513+shunkakinoki@users.noreply.github.com>
2021-12-26 15:46:18 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -95,6 +95,12 @@ const config = {
         permanent: true,
       },
       {
+        source: "/news",
+        destination:
+          "https://shunkakinoki.notion.site/0d7baa7e42434b5ea9cc8f2f73fe6e8f",
+        permanent: true,
+      },
+      {
         source: "/vote",
         destination: "https://vote.shunkakinoki.com",
         permanent: true,
