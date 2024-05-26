e9911e9ef9c03e7d34ce7c1c5bde9f47273f60b1
Shun Kakinoki <shunkakinoki@gmail.com>
2024-05-27 00:34:23 +0900

    chore: ini shadcn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,14 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Inter as FontSans } from "next/font/google"
+
+import { cn } from "@/lib/utils"
+
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-});
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -23,7 +22,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}>
         {children}
       </body>
     </html>
