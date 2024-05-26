390254bfc83de8fbd5abc6aa0c458c65d1a6d9db
Shun Kakinoki <shunkakinoki@gmail.com>
2024-05-27 00:57:02 +0900

    chore: ini shadcn

---
+++ src/config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+  name: "shunkakinoki",
+  url: "https://shunkakinoki.com",
+  ogImage: "https://shunkakinoki.com/og.jpg",
+  description:
+    "Shun Kakinoki's Personal Website",
+  links: {
+    twitter: "https://twitter.com/shunkakinoki",
+    github: "https://github.com/shunkakinoki",
+  },
+}
+
+export type SiteConfig = typeof siteConfig
