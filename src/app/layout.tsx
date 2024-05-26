dfc6915e0ec803d11f96687d76394166fb5b35f7
Shun Kakinoki <shunkakinoki@gmail.com>
2024-05-27 01:30:20 +0900

    chore: update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,100 +1,100 @@
-import { Metadata, Viewport } from "next"
+import { Metadata, Viewport } from "next";
 
-import { Analytics } from "@/components/analytics"
-import { ThemeProvider } from "@/components/providers"
-import { SiteHeader } from "@/components/site-header"
-import { TailwindIndicator } from "@/components/tailwind-indicator"
-import { Toaster } from "@/components/ui/sonner"
-import { siteConfig } from "@/config/site"
-import { fontSans } from "@/lib/fonts"
-import { cn } from "@/lib/utils"
-import "@/styles/globals.css"
+import { Analytics } from "@/components/analytics";
+import { ThemeProvider } from "@/components/providers";
+import { SiteHeader } from "@/components/site-header";
+import { TailwindIndicator } from "@/components/tailwind-indicator";
+import { Toaster } from "@/components/ui/sonner";
+import { siteConfig } from "@/config/site";
+import { fontSans } from "@/lib/fonts";
+import { cn } from "@/lib/utils";
+import "@/styles/globals.css";
 
 export const metadata: Metadata = {
-  title: {
-    default: siteConfig.name,
-    template: `%s - ${siteConfig.name}`,
-  },
-  metadataBase: new URL(siteConfig.url),
-  description: siteConfig.description,
-  authors: [
-    {
-      name: "shunkakinoki",
-      url: "https://shunkakinoki.com",
-    },
-  ],
-  creator: "shunkakinoki",
-  openGraph: {
-    type: "website",
-    locale: "en_US",
-    url: siteConfig.url,
-    title: siteConfig.name,
-    description: siteConfig.description,
-    siteName: siteConfig.name,
-    images: [
-      {
-        url: siteConfig.ogImage,
-        width: 1200,
-        height: 630,
-        alt: siteConfig.name,
-      },
-    ],
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: siteConfig.name,
-    description: siteConfig.description,
-    images: [siteConfig.ogImage],
-    creator: "@shunkakinoki",
-  },
-  icons: {
-    icon: "/favicon.ico",
-    shortcut: "/favicon-16x16.png",
-    apple: "/apple-touch-icon.png",
-  },
-  manifest: `${siteConfig.url}/site.webmanifest`,
-}
+	title: {
+		default: siteConfig.name,
+		template: `%s - ${siteConfig.name}`,
+	},
+	metadataBase: new URL(siteConfig.url),
+	description: siteConfig.description,
+	authors: [
+		{
+			name: "shunkakinoki",
+			url: "https://shunkakinoki.com",
+		},
+	],
+	creator: "shunkakinoki",
+	openGraph: {
+		type: "website",
+		locale: "en_US",
+		url: siteConfig.url,
+		title: siteConfig.name,
+		description: siteConfig.description,
+		siteName: siteConfig.name,
+		images: [
+			{
+				url: siteConfig.ogImage,
+				width: 1200,
+				height: 630,
+				alt: siteConfig.name,
+			},
+		],
+	},
+	twitter: {
+		card: "summary_large_image",
+		title: siteConfig.name,
+		description: siteConfig.description,
+		images: [siteConfig.ogImage],
+		creator: "@shunkakinoki",
+	},
+	icons: {
+		icon: "/favicon.ico",
+		shortcut: "/favicon-16x16.png",
+		apple: "/apple-touch-icon.png",
+	},
+	manifest: `${siteConfig.url}/site.webmanifest`,
+};
 
 export const viewport: Viewport = {
-  themeColor: [
-    { media: "(prefers-color-scheme: light)", color: "white" },
-    { media: "(prefers-color-scheme: dark)", color: "black" },
-  ],
-}
+	themeColor: [
+		{ media: "(prefers-color-scheme: light)", color: "white" },
+		{ media: "(prefers-color-scheme: dark)", color: "black" },
+	],
+};
 
 interface RootLayoutProps {
-  children: React.ReactNode
+	children: React.ReactNode;
 }
 
 export default function RootLayout({ children }: RootLayoutProps) {
-  return (
-    <>
-      <html lang="en" suppressHydrationWarning>
-        <head />
-        <body
-          className={cn(
-            "min-h-screen bg-background font-sans antialiased",
-            fontSans.variable
-          )}
-        >
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
-            <div vaul-drawer-wrapper="">
-            <SiteHeader />
-              <div className="relative flex min-h-screen flex-col bg-background">
-                {children}
-              </div>
-            </div>
-            <TailwindIndicator />
-            <Analytics />
-            <Toaster />
-          </ThemeProvider>
-        </body>
-      </html>
-    </>
-  )
+	return (
+		<>
+			<html lang="en" suppressHydrationWarning>
+				<head />
+				<body
+					className={cn(
+						"min-h-screen bg-background font-sans antialiased",
+						fontSans.variable,
+					)}
+				>
+					<ThemeProvider
+						attribute="class"
+						defaultTheme="system"
+						enableSystem
+						disableTransitionOnChange
+					>
+						<div vaul-drawer-wrapper="">
+							<SiteHeader />
+							<div className="relative flex min-h-screen flex-col bg-background">
+								{children}
+							</div>
+						</div>
+						<TailwindIndicator />
+						<Analytics />
+						<Toaster />
+					</ThemeProvider>
+				</body>
+			</html>
+		</>
+	);
 }
