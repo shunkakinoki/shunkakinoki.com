42b288d7bc21f28622f7908ad6d8cda8d6b84549
Shun Kakinoki <shunkakinoki@gmail.com>
2024-05-27 15:46:22 +0900

    chore: update section

--- src/components/life.tsx
+++ src/components/life.tsx
@@ -6,6 +6,8 @@ import {
 } from "@heroicons/react/24/outline";
 import Link from "next/link";
 
+import { SectionHeaderHeading } from "./section-header";
+
 const actions = [
   {
     title: "Cause",
@@ -29,46 +31,49 @@ const actions = [
 
 export function Life() {
   return (
-    <section className="divide-y divide-gray-200 overflow-hidden rounded-lg shadow sm:grid sm:grid-cols-3 sm:gap-px sm:divide-y-0 sm:divide-x">
-      {actions.map((action, actionIdx) => (
-        <div
-          key={action.title}
-          className={cn(
-            actionIdx === 0 ? "sm:rounded-l-lg" : "",
-            actionIdx === actions.length - 1 ? "sm:rounded-r-lg" : "",
-            "group relative p-6 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500 hover:bg-gray-900",
-          )}
-        >
-          <div>
+    <section>
+      <SectionHeaderHeading>Life</SectionHeaderHeading>
+      <div className="divide-y divide-gray-200 overflow-hidden rounded-lg shadow sm:grid sm:grid-cols-3 sm:gap-px sm:divide-y-0 sm:divide-x">
+        {actions.map((action, actionIdx) => (
+          <div
+            key={action.title}
+            className={cn(
+              actionIdx === 0 ? "sm:rounded-l-lg" : "",
+              actionIdx === actions.length - 1 ? "sm:rounded-r-lg" : "",
+              "group relative p-6 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500 hover:bg-gray-900",
+            )}
+          >
+            <div>
+              <span
+                className={cn(
+                  action.iconBackground,
+                  "inline-flex rounded-lg p-3",
+                )}
+              >
+                <action.icon className="h-6 w-6" aria-hidden="true" />
+              </span>
+            </div>
+            <div className="mt-8">
+              <h3 className="text-base font-semibold leading-6 text-foreground/80">
+                <Link href={action.href} className="focus:outline-none">
+                  {/* Extend touch target to entire panel */}
+                  <span className="absolute inset-0" aria-hidden="true" />
+                  {action.title}
+                </Link>
+              </h3>
+            </div>
             <span
-              className={cn(
-                action.iconBackground,
-                "inline-flex rounded-lg p-3",
-              )}
+              className="pointer-events-none absolute right-6 top-6 text-gray-300 group-hover:text-gray-400"
+              aria-hidden="true"
             >
-              <action.icon className="h-6 w-6" aria-hidden="true" />
+              {/* biome-ignore lint/a11y/noSvgWithoutTitle: <explanation> */}
+              <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
+                <path d="M20 4h1a1 1 0 00-1-1v1zm-1 12a1 1 0 102 0h-2zM8 3a1 1 0 000 2V3zM3.293 19.293a1 1 0 101.414 1.414l-1.414-1.414zM19 4v12h2V4h-2zm1-1H8v2h12V3zm-.707.293l-16 16 1.414 1.414 16-16-1.414-1.414z" />
+              </svg>
             </span>
           </div>
-          <div className="mt-8">
-            <h3 className="text-base font-semibold leading-6 text-foreground/80">
-              <Link href={action.href} className="focus:outline-none">
-                {/* Extend touch target to entire panel */}
-                <span className="absolute inset-0" aria-hidden="true" />
-                {action.title}
-              </Link>
-            </h3>
-          </div>
-          <span
-            className="pointer-events-none absolute right-6 top-6 text-gray-300 group-hover:text-gray-400"
-            aria-hidden="true"
-          >
-            {/* biome-ignore lint/a11y/noSvgWithoutTitle: <explanation> */}
-            <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
-              <path d="M20 4h1a1 1 0 00-1-1v1zm-1 12a1 1 0 102 0h-2zM8 3a1 1 0 000 2V3zM3.293 19.293a1 1 0 101.414 1.414l-1.414-1.414zM19 4v12h2V4h-2zm1-1H8v2h12V3zm-.707.293l-16 16 1.414 1.414 16-16-1.414-1.414z" />
-            </svg>
-          </span>
-        </div>
-      ))}
+        ))}
+      </div>
     </section>
   );
 }
