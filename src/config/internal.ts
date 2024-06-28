160e34e7201ffbaacf1d84a0f3e861db9558b795
Shun Kakinoki <shunkakinoki@gmail.com>
2024-06-28 18:35:55 +0900

    chore: update

---
+++ src/config/internal.ts
@@ -0,0 +1,18 @@
+// -----------------------------------------------------------------------------
+// Const
+// -----------------------------------------------------------------------------
+
+export const internalConfig = [
+  {
+    name: "Cal",
+    href: "https://cal.com/shunkakinoki",
+  },
+  {
+    name: "Meeting",
+    href: "https://cal.com/shunkakinoki/meeting",
+  },
+  {
+    name: "Journal",
+    href: "https://shunkakinoki.notion.site/4808e1f4b9074ceda5d8e17371103f10",
+  },
+];
