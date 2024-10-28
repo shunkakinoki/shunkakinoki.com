2a30dfdbeb055d26e941d94c23f65e9da0352b15
Shun Kakinoki <shunkakinoki@gmail.com>
2024-10-29 01:10:23 +0900

    chore: update

--- next.config.mjs
+++ next.config.mjs
@@ -34,6 +34,9 @@ const nextConfig = {
     ppr: "incremental",
     // biome-ignore lint/style/useNamingConvention: <explanation>
     dynamicIO: true,
+    turbo: {
+      treeShaking: true,
+    },
   },
   logging: {
     fetches: {
@@ -49,15 +52,6 @@ const nextConfig = {
       },
     ],
   },
-  // From: https://github.com/vercel/next.js/issues/65447#issuecomment-2294801418
-  pageExtensions: [
-    "page.tsx",
-    // FIXME: Next.js has a bug which does not resolve not-found.page.tsx correctly
-    // Instead, use `not-found.ts` as a workaround
-    // "ts" is required to resolve `not-found.ts`
-    // https://github.com/vercel/next.js/issues/65447
-    "ts",
-  ],
   // biome-ignore lint/suspicious/useAwait: <explanation>
   redirects: async () => {
     return [
